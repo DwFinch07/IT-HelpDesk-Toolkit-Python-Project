1f80441b855b7c90265218d4395deeb8e9d425a2
Dominic Finch <123dfinch@gmail.com>
2025-10-21 23:24:47 -0700

    Swaped to customtkinter

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import tkinter as tk
+import customtkinter as tk
 import os
 from ping_functions import *
 #Main Tkinter file (will have main GUI functionality, button presses will use Functions created in other files ex. ping.py)
 
-root = tk.Tk()
+root = tk.CTk()
 root.title("HelpDesk Toolkit")
 root.configure(bg="#494652")
 root.geometry("800x500")
@@ -12,11 +12,11 @@ def ping_on_click():
     result = test_ping
     return result
 #Ping Button
-ping_button = tk.Button(root, text = "Test Internet Connection",command = ping_on_click(),bg="#494652")
+ping_button = tk.CTkButton(root, text = "Test Internet Connection",command = ping_on_click())
 ping_button.place(x=200,y=200)
 
 #Quit Button
-quit_button = tk.Button(root, text = "Quit", command = root.destroy, bg="#494652")
+quit_button = tk.CTkButton(root, text = "Quit", command = root.destroy)
 quit_button.place(x=700,y=425)
 
 root.mainloop()
