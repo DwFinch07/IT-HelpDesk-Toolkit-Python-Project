63e57995c48f06a60039d1423eeec4843263d1fd
Dominic Finch <123dfinch@gmail.com>
2025-10-23 19:08:16 -0700

    Made output make sense
    Still have to do other todos, small edit.

--- resources.py
+++ resources.py
@@ -13,13 +13,13 @@ class Resources:
         info["OS Version"] = platform.version()
         info["CPU Cores"] = psutil.cpu_count(logical=False)
         info["Logical CPU Cores"] = psutil.cpu_count(logical=True)
-        info["CPU Usage"] = psutil.cpu_percent(interval=1)
-        info["Disk Usage"] = psutil.disk_usage('/')
-        info["Battery Usage"] = psutil.sensors_battery()
-
         cpu_freq = psutil.cpu_freq()
         if cpu_freq:
             info["CPU Freq (MHz)"] = round(cpu_freq.current,2)
+        info["Current CPU Usage"] = psutil.cpu_percent(interval=1)
+        info["Disk Usage"] = psutil.disk_usage('/')
+        info["Battery Usage"] = psutil.sensors_battery()
+
+
         
         return info
-print(Resources.windows_stats)
